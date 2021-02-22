3dec58b48637a12fb12317ca525095c364dae2cc
LeeJeaWon <73148498+LeeJeaWon@users.noreply.github.com>
2021-02-22 13:15:54 +0900

    useInput

---
+++ useInput.js
@@ -0,0 +1,19 @@
+const useInput = (initialvalue, validator) => {
+    const [value, setvalue] = useState(initialvalue);
+    const onChange = (event) => {
+        const {
+            target:{value}
+        } = event;
+
+        let willUpdate = true
+
+        if(typeof validator ==='function'){
+        willUpdate = validator(value)
+        }
+        
+        if(willUpdate){
+            setvalue(value)
+        }
+    };
+    return { value, onChange };
+};
